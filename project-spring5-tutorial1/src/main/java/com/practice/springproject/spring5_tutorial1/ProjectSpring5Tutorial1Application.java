5b15736551165151d3dded17e46f24d9489c01d1
Venkatesh <mavenka@apac.corpdir.net>
2018-01-31 10:07:24 +0530

    demo projects

---
+++ project-spring5-tutorial1/src/main/java/com/practice/springproject/spring5_tutorial1/ProjectSpring5Tutorial1Application.java
@@ -0,0 +1,12 @@
+package com.practice.springproject.spring5_tutorial1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpring5Tutorial1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpring5Tutorial1Application.class, args);
+	}
+}
---
+++ project-spring5-tutorial1/src/test/java/com/practice/springproject/spring5_tutorial1/ProjectSpring5Tutorial1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.practice.springproject.spring5_tutorial1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectSpring5Tutorial1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
